e39547439d9c173cff77e12a83e5fe9ce3f1d1ad
Aliz <fad127alireza2@gmail.com>
2020-10-06 17:27:20 +0330

    user can update account info and account picture (picture --> no logic)

--- views.py
+++ views.py
@@ -1,6 +1,7 @@
+import secrets
 from application import app , db , bcrypt
 from flask import render_template , redirect , flash , url_for , request
-from application.forms import RegisterForm , LoginForm
+from application.forms import RegisterForm , LoginForm , UpdateAccountForm
 from application.models import User , Post
 from flask_login import login_user , current_user , logout_user , login_required
 
@@ -19,6 +20,9 @@ posts = [
     }
 ]
 
+
+def save_picture(form_picture):
+    pass
 
 @app.route("/")
 @app.route("/home")
@@ -74,10 +78,25 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route("/account")
+@app.route("/account" , methods = ['GET' , 'POST'])
 @login_required
 def account():
-    response = render_template('account.html' , title = 'Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your Account info has been update!' , 'success')
+        return redirect('account') #problem for reload page solve (alert)
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    image_file = url_for ('static' , filename = 'profile_pics/'+ current_user.image_file)
+    response = render_template('account.html' , title = 'Account' , image_file = image_file , form = form)
     return response
 
 @app.route("/show-db")
