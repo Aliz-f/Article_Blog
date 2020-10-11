85f6d2f869b29ee352f2ab9381e55bf7655bdfe4
Aliz <fad127alireza2@gmail.com>
2020-10-11 17:10:40 +0330

    function for create_app

--- __init__.py
+++ __init__.py
@@ -1,30 +1,33 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from application.config import config
 
-file_dir = os.path.dirname(__file__)
-goual_route = os.path.join(os.path.join(file_dir,'database') , 'article-database.db')
-
-
-app = Flask(__name__)
-app.config.from_object(config)
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'users.login' #function-name
 login_manager.login_message_category = 'info' #bootstarp class
-mail = Mail(app)
+mail = Mail()
 
-import application.models
+def create_app (config_class = config):
+    app = Flask(__name__)
+    app.config.from_object(config)
 
-from application.users.views import users
-from application.posts.views import posts
-from application.main.views import main
+    bcrypt.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
 
-app.register_blueprint(users)
-app.register_blueprint(posts)
-app.register_blueprint(main)
+    from application.users.views import users
+    from application.posts.views import posts
+    from application.main.views import main
+
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+    
+    return app
+
